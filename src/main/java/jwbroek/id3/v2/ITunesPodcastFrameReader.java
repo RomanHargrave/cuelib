fa212a2628fc06b21b04d00723916d0fe68ceb93
Roman Hargrave <roman@hargrave.info>
2015-01-13 13:22:54 -0600

    Reformat code, remove hardcoded path.

--- src/main/java/jwbroek/id3/v2/ITunesPodcastFrameReader.java
+++ src/main/java/jwbroek/id3/v2/ITunesPodcastFrameReader.java
@@ -3,31 +3,27 @@
  */
 package jwbroek.id3.v2;
 
+import jwbroek.id3.ITunesPodcastFrame;
+
 import java.io.IOException;
 import java.io.InputStream;
 
-import jwbroek.id3.ITunesPodcastFrame;
+public class ITunesPodcastFrameReader implements FrameReader {
 
-public class ITunesPodcastFrameReader implements FrameReader
-{
-  private final int headerSize;
-  
-  public ITunesPodcastFrameReader(final int headerSize)
-  {
-    this.headerSize = headerSize;
-  }
-  
-  public ITunesPodcastFrame readFrameBody(final int size, final InputStream input)
-      throws IOException, UnsupportedEncodingException, MalformedFrameException
-  {
-    final ITunesPodcastFrame result = new ITunesPodcastFrame(this.headerSize + size);
-    final StringBuilder payloadBuilder = new StringBuilder();
-    for (int index = 0; index < 4; index++)
-    {
-      payloadBuilder.append(Integer.toHexString(input.read()));
+    private final int headerSize;
+
+    public ITunesPodcastFrameReader(final int headerSize) {
+        this.headerSize = headerSize;
     }
-    result.setPayload(payloadBuilder.toString());
-    return result;
-  }
+
+    public ITunesPodcastFrame readFrameBody(final int size, final InputStream input) throws IOException, UnsupportedEncodingException, MalformedFrameException {
+        final ITunesPodcastFrame result = new ITunesPodcastFrame(this.headerSize + size);
+        final StringBuilder payloadBuilder = new StringBuilder();
+        for (int index = 0; index < 4; index++) {
+            payloadBuilder.append(Integer.toHexString(input.read()));
+        }
+        result.setPayload(payloadBuilder.toString());
+        return result;
+    }
 
 }
