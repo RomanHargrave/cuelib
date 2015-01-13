8dfeb8423960464012ada452d4ec2731df9bedc3
Roman Hargrave <roman@hargrave.info>
2015-01-13 13:23:10 -0600

    Reformat code

--- src/main/java/jwbroek/id3/v2/MalformedFrameException.java
+++ src/main/java/jwbroek/id3/v2/MalformedFrameException.java
@@ -18,30 +18,23 @@
  */
 package jwbroek.id3.v2;
 
-public class MalformedFrameException extends Exception
-{
-  /**
-   * 
-   */
-  private static final long serialVersionUID = 6928538930151585090L;
+public class MalformedFrameException extends Exception {
 
-  public MalformedFrameException()
-  {
-    super();
-  }
+    private static final long serialVersionUID = 6928538930151585090L;
 
-  public MalformedFrameException(String message)
-  {
-    super(message);
-  }
+    public MalformedFrameException() {
+        super();
+    }
 
-  public MalformedFrameException(String message, Throwable cause)
-  {
-    super(message, cause);
-  }
+    public MalformedFrameException(String message) {
+        super(message);
+    }
 
-  public MalformedFrameException(Throwable cause)
-  {
-    super(cause);
-  }
+    public MalformedFrameException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MalformedFrameException(Throwable cause) {
+        super(cause);
+    }
 }
