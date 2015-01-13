13377ba5308c5e37c737e708dc6c1aeb02935bd7
Roman Hargrave <roman@hargrave.info>
2015-01-13 13:23:25 -0600

    Reformat code

--- src/main/java/jwbroek/id3/v2/MCIFrameReader.java
+++ src/main/java/jwbroek/id3/v2/MCIFrameReader.java
@@ -18,32 +18,28 @@
  */
 package jwbroek.id3.v2;
 
+import jwbroek.id3.MusicCDIdentifierFrame;
+
 import java.io.IOException;
 import java.io.InputStream;
 
-import jwbroek.id3.MusicCDIdentifierFrame;
+public class MCIFrameReader implements FrameReader {
 
-public class MCIFrameReader implements FrameReader
-{
-  private final int headerSize;
-  
-  public MCIFrameReader(final int headerSize)
-  {
-    this.headerSize = headerSize;
-  }
-  
-  public MusicCDIdentifierFrame readFrameBody(final int size, final InputStream input)
-    throws IOException, UnsupportedEncodingException, MalformedFrameException
-  {
-    final MusicCDIdentifierFrame result = new MusicCDIdentifierFrame();
-    result.setTotalFrameSize(size + headerSize);
-    final StringBuilder hexBuilder = new StringBuilder();
-    for (int index = 0; index < size; index++)
-    {
-      hexBuilder.append(Integer.toHexString(input.read()));
+    private final int headerSize;
+
+    public MCIFrameReader(final int headerSize) {
+        this.headerSize = headerSize;
     }
-    result.setHexTOC(hexBuilder.toString());
-    
-    return result;
-  }
+
+    public MusicCDIdentifierFrame readFrameBody(final int size, final InputStream input) throws IOException, UnsupportedEncodingException, MalformedFrameException {
+        final MusicCDIdentifierFrame result = new MusicCDIdentifierFrame();
+        result.setTotalFrameSize(size + headerSize);
+        final StringBuilder hexBuilder = new StringBuilder();
+        for (int index = 0; index < size; index++) {
+            hexBuilder.append(Integer.toHexString(input.read()));
+        }
+        result.setHexTOC(hexBuilder.toString());
+
+        return result;
+    }
 }
